b40b63743412652f9a4bbcc477a083058709312c
Segni Mideksa <segnim21@gmail.com>
2026-02-02 23:18:06 +0300

    modify js file

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // Create remove button
         const removeButton = document.createElement("button");
         removeButton.textContent = "Remove";
-        removeButton.className = "remove-btn";
+        removeButton.classList.add ("remove-btn");
 
         // Remove task when button is clicked
         removeButton.onclick = function () {
